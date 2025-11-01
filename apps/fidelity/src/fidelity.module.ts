3d0b1587c417f4b69dbb6101e4d4cc277e5f395e
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-10-31 22:33:25 -0300

    feat: :sparkles: adicionar fidelity_database_url

--- apps/fidelity/src/fidelity.module.ts
+++ apps/fidelity/src/fidelity.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { FidelityController } from '@fidelity/fidelity.controller';
 import { BonusModule } from '@fidelity/modules/bonus/bonus.module';
 import { PrismaService } from '@fidelity/infra/database/prisma.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [BonusModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        FIDELITY_DATABASE_URL: Joi.string().uri().required(),
+      }),
+    }),
+    BonusModule,
+  ],
   controllers: [FidelityController],
   providers: [PrismaService],
 })
