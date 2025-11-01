021f256554f7e23acd4c1eecf64e790932c020bb
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-10-31 22:26:16 -0300

    fix: :bug: corrigir import de bonus repository

--- apps/fidelity/src/infra/repositories/bonus.repository.ts
+++ apps/fidelity/src/infra/repositories/bonus.repository.ts
@@ -1,4 +1,4 @@
-import { BonusRepository } from "@fidelity/domains/repositories/Bonus.repository";
+import { BonusRepository } from "@fidelity/domains/repositories/bonus.repository";
 import { PrismaService } from "@fidelity/infra/database/prisma.service";
 
 export class PrismaBonusRepository implements BonusRepository {
