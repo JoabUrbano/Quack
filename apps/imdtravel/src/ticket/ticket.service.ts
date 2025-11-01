45ed9a8cb48e8a138bfab4a24310d1ec78fb5e4e
JoabUrbano <joab.uaraujo@gmail.com>
2025-10-31 23:12:59 -0300

    fidelityGateway

--- apps/imdtravel/src/ticket/ticket.service.ts
+++ apps/imdtravel/src/ticket/ticket.service.ts
@@ -3,22 +3,30 @@ import { BuyTicketDto } from '@imdtravel/ticket/dtos/buyTicket.dto';
 import { AirlineHubGateway } from '@app/shared';
 import { ExchangeGateway } from '@app/shared/exchange.gateway';
 import { FlightDto } from '@app/shared/types/fligt.dto';
+import { FidelityGateway } from '@app/shared/fidelity.gateway';
 
 @Injectable()
 export class TicketService {
   constructor(
     private airlineHubGateway: AirlineHubGateway,
     private exchangeGateway: ExchangeGateway,
+    private fidelityGateway: FidelityGateway
   ) {}
 
   async buyTicket(input: BuyTicketDto): Promise<FlightDto> {
-    const { flight: flightNumber, day } = input;
+    const { flight: flightNumber, day, userId } = input;
 
     const flight = await this.airlineHubGateway.getFlight(flightNumber, day);
 
     const exchange = await this.exchangeGateway.getRandomNumberExchange();
 
     const realToDolar = Math.round(flight.value / exchange);
+    const bonusParams = {
+      value: flight.value*1/10,
+      user: userId
+    }
+    const bonus = await this.fidelityGateway.createBonus(bonusParams);
+    console.log(bonus);
 
     return {
       ...flight,
--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AirlineHubGateway } from './airlineshub.gateway';
 import { HttpModule } from '@nestjs/axios';
 import { ExchangeGateway } from './exchange.gateway';
+import { FidelityGateway } from './fidelity.gateway';
 
 @Module({
   imports: [HttpModule],
-  providers: [AirlineHubGateway, ExchangeGateway],
-  exports: [AirlineHubGateway, ExchangeGateway],
+  providers: [AirlineHubGateway, ExchangeGateway, FidelityGateway],
+  exports: [AirlineHubGateway, ExchangeGateway, FidelityGateway],
 })
 export class SharedModule {}
