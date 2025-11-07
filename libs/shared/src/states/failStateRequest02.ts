6018bab38b9ed573ef114d8c32bfba079e04f249
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-11-07 18:57:55 -0300

    feat: resetar timeout apos um evento de falha

--- libs/shared/src/states/failStateRequest02.ts
+++ libs/shared/src/states/failStateRequest02.ts
@@ -1,19 +1,28 @@
-import { Injectable } from "@nestjs/common";
-import { probabilityEvent } from "../utils/probability";
+import { Injectable } from '@nestjs/common';
+import { probabilityEvent } from '../utils/probability';
 
 @Injectable()
-export class FailStateRequest02{
-     public request02State: boolean = false
-    constructor() {}
+export class FailStateRequest02 {
+  public request02State: boolean = false;
+  public timeout: NodeJS.Timeout;
 
-    probability() {
-        const event = probabilityEvent(10);
-        
-        if(event) {
-            this.request02State = true
-        }
-        setTimeout(() => {
-            this.request02State = false
-        }, 5000)
+  constructor() {}
+
+  probability() {
+    const event = probabilityEvent(10);
+
+    if (!event) return;
+
+    this.request02State = true;
+
+    if (this.timeout) {
+      console.log('[FailStateRequest02] clearing timeout');
+
+      clearTimeout(this.timeout);
     }
+
+    this.timeout = setTimeout(() => {
+      this.request02State = false;
+    }, 5000);
+  }
 }
