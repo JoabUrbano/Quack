f3e81d3552414a6a29505067e33a13f7b1b3d4d8
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-11-24 21:35:09 -0300

    feat: adicionar configuracao exchange e fidelity no kong config

--- apps/exchange/src/exchange.module.ts
+++ apps/exchange/src/exchange.module.ts
@@ -3,9 +3,12 @@ import { ExchangeController } from '@exchange/exchange.controller';
 import { ExchangeService } from '@exchange/exchange.service';
 import { RandomExchangeModule } from '@exchange/modules/randomExchange/randomExchange.module';
 import { SharedModule } from '@app/shared';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RandomExchangeModule, SharedModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true
+  }), RandomExchangeModule, SharedModule],
   controllers: [ExchangeController],
   providers: [ExchangeService],
 })
