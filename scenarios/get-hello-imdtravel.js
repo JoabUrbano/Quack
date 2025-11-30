e73c950acc445d95d16cc4b42a781009cb2bb1c3
luizgustavoou <luizgustavooumbelino@gmail.com>
2025-11-30 20:46:45 -0300

    feat: adicionar estagios no teste basico do hello do imdtravel

--- scenarios/get-hello-imdtravel.js
+++ scenarios/get-hello-imdtravel.js
@@ -1,10 +1,34 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+// export const options = {
+//     iterations: 10,
+//     vus: 5,
+// }
+
+
+/**
+ * target: number of virtual users
+ * duration: time to stay at that level
+ */
 export const options = {
-    iterations: 10,
-    vus: 5,
+    stages: [
+        {
+            duration: '30s',
+            target: 20
+        },
+        {
+            duration: '10s',
+            target: 10
+        },
+        {
+            duration: '20s',
+            target: 0
+        }
+    ]
 }
+
+
 
 export default function() {
     http.get('http://localhost:8000/imdtravel')
